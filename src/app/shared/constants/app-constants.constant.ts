6660c3cf181257b9d0598314ef437e68d089cdd9
Christoffer Skog <christofferskog@ChristoffersMBP.lan>
2017-07-23 18:26:25 +0200

    Added app prefix as an export

--- src/app/shared/constants/app-constants.constant.ts
+++ src/app/shared/constants/app-constants.constant.ts
@@ -3,11 +3,13 @@ import {environment} from '../../../environments/environment';
 const AppPrefix = 'myApp';
 
 class IAppConstants {
+  prefix: string;
   host: string;
   token: string;
 }
 
 export const AppConstants: IAppConstants = {
+  prefix: AppPrefix,
   host: environment.production ? 'https://my-prod-api.com/' : 'https://my-dev-api.com',
   token: `${AppPrefix}.token`
 };
