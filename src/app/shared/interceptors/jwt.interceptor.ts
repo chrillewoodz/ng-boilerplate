c031b36d4c0e86fb377e267cb179352ee4cb8bad
Christoffer Skog <christofferskog@ChristoffersMBP.lan>
2017-08-01 14:42:28 +0200

    Replaced Http with HttpClient

---
+++ src/app/shared/interceptors/jwt.interceptor.ts
@@ -0,0 +1,24 @@
+import {HttpRequest, HttpHandler, HttpEvent, HttpInterceptor} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+
+import {AppConstants} from '@constants/app-constants.constant';
+import {ClientStorage} from '@services/client-storage.service';
+
+@Injectable()
+
+export class JWTInterceptor implements HttpInterceptor {
+
+  constructor(private cs: ClientStorage) {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+    req = req.clone({
+      setHeaders: {
+        authorization: `Bearer ${this.cs.getItem(AppConstants.token)}`
+      }
+    });
+
+    return next.handle(req);
+  }
+}
---
+++ src/app/shared/interceptors/providers.ts
@@ -0,0 +1,5 @@
+import {JWTInterceptor} from './jwt.interceptor';
+
+export const INTERCEPTORS_PROVIDERS: any[] = [
+  JWTInterceptor
+];
